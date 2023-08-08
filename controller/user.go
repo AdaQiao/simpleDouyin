9ecc11746ab2f89956a23a6314b9ca1b7238d67b
AdaQiao <1243282726@qq.com>
2023-08-08 16:54:09 +0800

    q

--- controller/user.go
+++ controller/user.go
@@ -89,16 +89,16 @@ func Login(c *gin.Context) {
 }
 
 func UserInfo(c *gin.Context) {
-	//token := c.Query("token")
-	//
-	//if user, exist := UsersLoginInfo[token]; exist {
-	//	c.JSON(http.StatusOK, UserResponse{
-	//		Response: Response{StatusCode: 0},
-	//		User:     user,
-	//	})
-	//} else {
-	//	c.JSON(http.StatusOK, UserResponse{
-	//		Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-	//	})
-	//}
+	token := c.Query("token")
+
+	if user, exist := UsersLoginInfo[token]; exist {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 0},
+			User:     user,
+		})
+	} else {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+		})
+	}
 }
