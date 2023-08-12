78233ddacdbb3b28601db150e45343267bf6c1f4
Jiao-Yiyang <1391079660@qq.com>
2023-08-12 22:44:25 +0800

    query by id

--- model/demo_data.go
+++ model/demo_data.go
@@ -2,7 +2,7 @@ package model
 
 var DemoVideos = []Video{
 	{
-		Id:            1,
+		Id:            5,
 		Author:        DemoUser,
 		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
 		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
