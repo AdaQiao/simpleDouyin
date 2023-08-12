01720908a2873bd5c4868b4543227bc67f56338f
Jiao-Yiyang <1391079660@qq.com>
2023-08-12 12:00:28 +0800

    change name

--- db/VideoRepository.go
+++ db/VideoRepository.go
@@ -4,5 +4,5 @@ import "github.com/RaymondCode/simple-demo/model"
 
 type VideoRepository interface {
 	CreateVideo(user model.Video) error
-	GetVideo(token string) (*model.Video, error)
+	GetVideoByToken(token string) (*model.Video, error)
 }
