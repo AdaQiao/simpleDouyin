ec401a9fc69afeeb53d82c5f79883b9c930e79cd
AdaQiao <1243282726@qq.com>
2023-08-12 10:49:06 +0800

    qq

--- service/publishService.go
+++ service/publishService.go
@@ -1,1 +1,52 @@
 package service
+
+import (
+	"fmt"
+	"github.com/RaymondCode/simple-demo/db"
+	"github.com/RaymondCode/simple-demo/model"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type PublishService interface {
+	Publish(token string, reply *model.Response) error
+	PublishList(reply *model.VideoListResponse)
+}
+
+var repo *db.MySQLUserRepository = db.NewMySQLUserRepository()
+
+type PublishServiceImpl struct {
+}
+
+func (s *PublishServiceImpl) Publish(token string, reply *model.Response) error {
+
+}
+func (s *PublishServiceImpl) PublishList(reply *model.VideoListResponse) {
+
+}
+
+func RunPublishServer() {
+	// 创建服务实例
+
+	publishService := &PublishServiceImpl{}
+
+	// 注册RPC服务
+	rpc.Register(publishService)
+
+	// 启动RPC服务器
+	listener, err := net.Listen("tcp", "127.0.0.1:9092")
+	if err != nil {
+		log.Fatal("RPC服务器启动失败:", err)
+	}
+
+	fmt.Println("RPC服务器已启动，等待远程调用...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("接受连接失败:", err)
+		}
+		go rpc.ServeConn(conn)
+	}
+}
