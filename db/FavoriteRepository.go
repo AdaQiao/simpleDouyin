47905f486bb0f5f3fbbe39e1ed475303be41e101
AdaQiao <1243282726@qq.com>
2023-08-17 16:39:11 +0800

    delete favorite id

--- db/FavoriteRepository.go
+++ db/FavoriteRepository.go
@@ -27,10 +27,11 @@ func (repo *MySQLFavoriteRepository) AddFavorite(userID, videoID int64) error {
 	defer repo.mutex.Unlock()
 
 	// 检查是否已存在相同的记录
-	query := "SELECT is_favorite FROM favorite WHERE user_id = ? AND video_id = ?"
+	query := "SELECT id, is_favorite FROM favorite WHERE user_id = ? AND video_id = ?"
 	row := dB.QueryRow(query, userID, videoID)
+	var id int64
 	var isFavorite int
-	err := row.Scan(&isFavorite)
+	err := row.Scan(&id, &isFavorite)
 	if err == sql.ErrNoRows {
 		// 不存在记录，插入新记录
 		query = "INSERT INTO favorite (user_id, video_id, is_favorite) VALUES (?, ?, 1)"
@@ -47,8 +48,8 @@ func (repo *MySQLFavoriteRepository) AddFavorite(userID, videoID int64) error {
 			return errors.New("已点赞")
 		}
 		// 已取消点赞，更新 is_favorite 为 1
-		query = "UPDATE favorite SET is_favorite = 1 WHERE user_id = ? AND video_id = ?"
-		_, err = dB.Exec(query, userID, videoID)
+		query = "UPDATE favorite SET is_favorite = 1 WHERE id = ?"
+		_, err = dB.Exec(query, id)
 		if err != nil {
 			log.Println("更新喜欢记录失败:", err)
 			return err
