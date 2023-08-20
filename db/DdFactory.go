8fae74101a55699030dbb8bc441f06f5f7aa2aa0
Chen <1731140705@qq.com>
2023-08-20 21:07:44 +0800

    dbUpdate

--- db/DdFactory.go
+++ db/DdFactory.go
@@ -9,7 +9,7 @@ import (
 
 func NewMySQLDB() (*sql.DB, error) {
 	// 设置数据库连接参数
-	db, err := sql.Open("mysql", "root:tCzAhYFo@tcp(172.16.32.4:53029)/simpleDouyin")
+	db, err := sql.Open("mysql", "root:tCzAhYFo@tcp(172.16.32.93:53398)/simpleDouyin")
 	if err != nil {
 		return nil, err
 	}
