45b34f4cd5b514e213acf482aa2daf0893dece67
Jiao-Yiyang <1391079660@qq.com>
2023-08-22 10:33:31 +0800

    change db

--- db/DdFactory.go
+++ db/DdFactory.go
@@ -17,7 +17,7 @@ func NewMySQLDB() (*sql.DB, error) {
 	// 测试数据库连接
 	err = db.Ping()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("连接数据库失败，原因是1024平台提供的mysql_HOST和mysql_PORT发生了改变，需要手动更新")
 	}
 
 	fmt.Println("MySQL数据库连接成功")
