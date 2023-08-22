ad85031071ee096ba89de3c7c52d26194ea5f064
Jiao-Yiyang <1391079660@qq.com>
2023-08-22 10:49:55 +0800

    change db

--- db/DdFactory.go
+++ db/DdFactory.go
@@ -3,13 +3,17 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewMySQLDB() (*sql.DB, error) {
 	// 设置数据库连接参数
-	db, err := sql.Open("mysql", "root:tCzAhYFo@tcp(172.16.32.108:49547)/simpleDouyin")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	fmt.Println(host)
+	db, err := sql.Open("mysql", "root:tCzAhYFo@tcp("+host+":"+port+")/simpleDouyin")
 	if err != nil {
 		return nil, fmt.Errorf("连接数据库失败，原因是1024平台提供的mysql_HOST和mysql_PORT发生了改变，需要手动更新")
 	}
