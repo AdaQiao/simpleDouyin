4081eccce999683f3adbfdfcaca16116f4f234a6
Jiao-Yiyang <1391079660@qq.com>
2023-08-30 09:26:13 +0800

    add

--- service/relationService.go
+++ service/relationService.go
@@ -101,19 +101,178 @@ func (s *RelationServiceImpl) FollowAction(req model.FollowActionMessage, reply 
 }
 
 func (s *RelationServiceImpl) FollowList(req model.RelationListMessage, reply *model.UserListResponse) error {
+	_, err := s.UserRepo.GetUserId(req.Token)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "user didn't exist",
+			},
+			UserList: nil,
+		}
+		return fmt.Errorf("user didn't uploaded")
+	}
+	followIds, err := s.RelationRepo.GetFollowById(req.UserId)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "get followList failed",
+			},
+			UserList: nil,
+		}
+		return nil
+	}
+
+	var userList = make([]model.User, len(followIds))
+	for i := 0; i < len(followIds); i++ {
+		tempUser, err := s.UserRepo.GetUserByUserId(followIds[i])
+		if err != nil {
+			*reply = model.UserListResponse{
+				Response: model.Response{
+					StatusCode: 1,
+					StatusMsg:  "获取关注用户失败",
+				},
+				UserList: nil,
+			}
+			return nil
+		}
+		tempUser.IsFollow = true
+		userList[i] = *tempUser
+	}
+
+	*reply = model.UserListResponse{
+		Response: model.Response{
+			StatusCode: 0,
+		},
+		UserList: userList,
+	}
 	return nil
 }
 func (s *RelationServiceImpl) FollowerList(req model.RelationListMessage, reply *model.UserListResponse) error {
+	_, err := s.UserRepo.GetUserId(req.Token)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "user didn't exist",
+			},
+			UserList: nil,
+		}
+		return fmt.Errorf("user didn't uploaded")
+	}
+	followerIds, err := s.RelationRepo.GetFollowerById(req.UserId)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "get followList failed",
+			},
+			UserList: nil,
+		}
+		return nil
+	}
+
+	var userList = make([]model.User, len(followerIds))
+	for i := 0; i < len(followerIds); i++ {
+		tempUser, err := s.UserRepo.GetUserByUserId(followerIds[i])
+		if err != nil {
+			*reply = model.UserListResponse{
+				Response: model.Response{
+					StatusCode: 1,
+					StatusMsg:  "获取粉丝用户失败",
+				},
+				UserList: nil,
+			}
+			return nil
+		}
+		tempUser.IsFollow, err = s.RelationRepo.CheckFollow(req.UserId, followerIds[i])
+		if err != nil {
+			*reply = model.UserListResponse{
+				Response: model.Response{
+					StatusCode: 1,
+					StatusMsg:  "获取粉丝用户失败",
+				},
+				UserList: nil,
+			}
+			return nil
+		}
+		userList[i] = *tempUser
+	}
+
+	*reply = model.UserListResponse{
+		Response: model.Response{
+			StatusCode: 0,
+		},
+		UserList: userList,
+	}
 	return nil
 }
 func (s *RelationServiceImpl) FriendList(req model.RelationListMessage, reply *model.UserListResponse) error {
+	_, err := s.UserRepo.GetUserId(req.Token)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "user didn't exist",
+			},
+			UserList: nil,
+		}
+		return fmt.Errorf("user didn't uploaded")
+	}
+	followerIds, err := s.RelationRepo.GetFollowerById(req.UserId)
+	if err != nil {
+		*reply = model.UserListResponse{
+			Response: model.Response{
+				StatusCode: 1,
+				StatusMsg:  "get followList failed",
+			},
+			UserList: nil,
+		}
+		return nil
+	}
+
+	var userList []model.User
+	for i := 0; i < len(followerIds); i++ {
+		tempUser, err := s.UserRepo.GetUserByUserId(followerIds[i])
+		if err != nil {
+			*reply = model.UserListResponse{
+				Response: model.Response{
+					StatusCode: 1,
+					StatusMsg:  "获取关注用户失败",
+				},
+				UserList: nil,
+			}
+			return nil
+		}
+		isFollow, err := s.RelationRepo.CheckFollow(req.UserId, followerIds[i])
+		if err != nil {
+			*reply = model.UserListResponse{
+				Response: model.Response{
+					StatusCode: 1,
+					StatusMsg:  "检查是否关注失败",
+				},
+				UserList: nil,
+			}
+			return nil
+		}
+		if isFollow {
+			userList = append(userList, *tempUser)
+		}
+
+	}
+
+	*reply = model.UserListResponse{
+		Response: model.Response{
+			StatusCode: 0,
+		},
+		UserList: userList,
+	}
 	return nil
 }
 
 type RelationServiceImpl struct {
 	UserRepo     *db.MySQLUserRepository
-	VideoRepo    *db.MySQLVideoRepository
-	FavoriteRepo *db.MySQLFavoriteRepository
 	RelationRepo *db.MySQLRelationRepository
 }
 
@@ -121,8 +280,6 @@ func RunRelationServer() {
 	// 创建服务实例
 	relationService := &RelationServiceImpl{
 		UserRepo:     db.NewMySQLUserRepository(),
-		VideoRepo:    db.NewMySQLVideoRepository(),
-		FavoriteRepo: db.NewMySQLFavoriteRepository(),
 		RelationRepo: db.NewMySQLRelationRepository(),
 	}
 
